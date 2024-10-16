52fb44cc522557ed5c6dc7b4225f9b3276e26ed3
yadenmezi <Yarden.Mezi@mail.huji.ac.il>
2024-10-16 16:51:06 +1000

    reformat again

--- sleepeegpy/tests/test_cleaning_pipeline.py
+++ sleepeegpy/tests/test_cleaning_pipeline.py
@@ -6,6 +6,7 @@ from sleepeegpy.dashboard import create_dashboard
 import numpy as np
 import mne
 
+
 def _basic_eeg_file_creation():
     sfreq = 250
     n_channels = 22
@@ -20,16 +21,40 @@ def _basic_eeg_file_creation():
         noise = np.random.normal(0, 0.05, size=times.shape)
         data[i] = alpha + beta + theta + noise
 
-    ch_names = ['Fp1', 'Fp2', 'Fz', 'F3', 'F4', 'F7', 'F8', 'FC1',
-                'FC2', 'Cz', 'C3', 'C4', 'T7', 'T8', 'Pz', 'P3',
-                'P4', 'P7', 'P8', 'O1', 'O2', 'Iz']
-    info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types=["eeg"] * n_channels)
+    ch_names = [
+        "Fp1",
+        "Fp2",
+        "Fz",
+        "F3",
+        "F4",
+        "F7",
+        "F8",
+        "FC1",
+        "FC2",
+        "Cz",
+        "C3",
+        "C4",
+        "T7",
+        "T8",
+        "Pz",
+        "P3",
+        "P4",
+        "P7",
+        "P8",
+        "O1",
+        "O2",
+        "Iz",
+    ]
+    info = mne.create_info(
+        ch_names=ch_names, sfreq=sfreq, ch_types=["eeg"] * n_channels
+    )
     return mne.io.RawArray(data, info)
+
 
 @pytest.fixture
 def setup_eeg_file(tmp_path):
     raw = _basic_eeg_file_creation()
-    montage = mne.channels.make_standard_montage('standard_1020')
+    montage = mne.channels.make_standard_montage("standard_1020")
     raw.set_montage(montage)
     eeg_file_path = tmp_path / "test_eeg_file_raw.fif"
     raw.save(eeg_file_path, overwrite=True)
@@ -63,7 +88,7 @@ def test_notch(setup_cleaning_pipe):
     cleaning_pipe = setup_cleaning_pipe
     cleaning_pipe.mne_raw = mne.io.read_raw_fif(cleaning_pipe.path_to_eeg, preload=True)
     original_data = cleaning_pipe.mne_raw.get_data()
-    cleaning_pipe.notch(freqs='50s')
+    cleaning_pipe.notch(freqs="50s")
     filtered_data = cleaning_pipe.mne_raw.get_data()
     assert original_data.shape == filtered_data.shape
 
@@ -72,7 +97,7 @@ def test_auto_detect_bad_channels(setup_cleaning_pipe):
     cleaning_pipe = setup_cleaning_pipe
     bad_channels_file = cleaning_pipe.auto_detect_bad_channels()
     assert os.path.isfile(bad_channels_file)
-    with open(bad_channels_file, 'r') as f:
+    with open(bad_channels_file, "r") as f:
         bad_channels = f.read().splitlines()
     assert isinstance(bad_channels, list)
 
@@ -80,18 +105,21 @@ def test_auto_detect_bad_channels(setup_cleaning_pipe):
 def test_save_annotations(setup_cleaning_pipe):
     cleaning_pipe = setup_cleaning_pipe
     cleaning_pipe.mne_raw = mne.io.read_raw_fif(cleaning_pipe.path_to_eeg, preload=True)
-    cleaning_pipe.mne_raw.annotations.append(onset=1, duration=1, description='bad')
-    annotations_file_path = cleaning_pipe.output_dir /"CleaningPipe"/ "annotations.txt"
+    cleaning_pipe.mne_raw.annotations.append(onset=1, duration=1, description="bad")
+    annotations_file_path = (
+        cleaning_pipe.output_dir / "CleaningPipe" / "annotations.txt"
+    )
     cleaning_pipe.save_annotations(overwrite=True)
-    assert os.path.isfile(annotations_file_path), f"File {annotations_file_path} not found."
+    assert os.path.isfile(
+        annotations_file_path
+    ), f"File {annotations_file_path} not found."
     loaded_annotations = mne.read_annotations(annotations_file_path)
     assert len(loaded_annotations) > 0, "loaded_annotations is 0"
 
+
 def test_dashboard(setup_cleaning_pipe):
-    fig = create_dashboard(
-        subject_code="EL3001",
-        prec_pipe=setup_cleaning_pipe
-    )
+    fig = create_dashboard(subject_code="EL3001", prec_pipe=setup_cleaning_pipe)
+
 
 if __name__ == "__main__":
     pytest.main()
